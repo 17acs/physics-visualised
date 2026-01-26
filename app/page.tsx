ccd5b67d2faf3479bbe3a0e2b5db4654ae51ec4f
17acs <healthycloroxbleach@gmail.com>
2026-01-26 22:05:16 +0000

    Update title screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ export default function Home() {
   return (
     <main style={{ padding: 32, fontFamily: "system-ui" }}>
       <h1 style={{ fontSize: 40, marginBottom: 8 }}>physics visualised</h1>
-      <p style={{ fontSize: 18, maxWidth: 700, textIndent: 20 }}>
+     <p style={{ fontSize: 18, maxWidth: 700, textIndent: 20 }}>
   personal portfolio by will king
 </p>
+
+<p style={{ fontSize: 18, maxWidth: 700, textIndent: 20, marginTop: 0 }}>
+  science isn't as boring as you think
+</p>
+
 
       <div style={{ marginTop: 24, display: "flex", gap: 12 }}>
         <a
