28b577a07399d310a17a222d346f733cc346af0e
17acs <healthycloroxbleach@gmail.com>
2026-02-03 23:35:11 +0000

    Polish home page and waves superposition UI

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
 
       <h1 style={{ fontSize: 32, marginBottom: 12 }}>About</h1>
       <p style={{ fontSize: 18, lineHeight: 1.6 }}>
-        I first started coding on roblox selling scripts to people for money. Lua was a lot easier than javascript so I'm doing this to work on my js skills as well as help provide diagrams for tricky scientific concepts. As of the 27th of Jan, the entire foundation of the website has been coded by yours truly, but complex models such as 3 dimension ones and some nuclear equations have been outsourced via AI or online code-sharing websites such as github. For inquiries contact ws.king@outlook.com.
+        I first started coding on roblox selling scripts. Lua was a lot easier than javascript so I'm doing this to work on my js skills as well as help provide diagrams for tricky scientific concepts. As of the 27th of Jan, the entire foundation of the website has been coded by me (hence why it's so basic but I'll improve it soon enough), but complex models such as 3D visuals and some nuclear equations have been outsourced via AI or online code-sharing websites such as github. Shout me at ws.king@outlook.com for inquiries
       </p>
     </main>
   );
